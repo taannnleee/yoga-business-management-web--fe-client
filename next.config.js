a242c0c65b65fdc819437bb71f31e77983e18d34
Tân <leetaan1902@gmail.com>
2024-12-17 22:40:28 +0700

    d

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-import { withNextVideo } from "next-video/process";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, // Disable React's strict mode
@@ -20,4 +18,5 @@ const nextConfig = {
     },
 };
 
-export default withNextVideo(nextConfig);
+// Sử dụng CommonJS (module.exports thay vì export default)
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,94 @@
-'use client'
-import HeroSection from "@/components/organisms/Hero";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
-import { redirect } from "next/navigation";
-
-import { useEffect, useLayoutEffect } from "react";
-import { useRouter } from "next/navigation";
-import HomePage from "@/app/(main)/home/page";
-
-let token: string | null = null;
-if (typeof window !== "undefined" && typeof window.localStorage !== "undefined") {
-    token = localStorage.getItem("accessToken");
-}
 export default function Home() {
-    const router = useRouter();
-
-    useLayoutEffect(() => {
-        // Kiểm tra nếu accessToken tồn tại trong localStorage
-
-        if (token) {
-            // Nếu tồn tại, redirect đến "/home"
-            router.push("/home");
-        } else {
-            // Nếu không, redirect đến "/login"
-            router.push("/login");
-        }
-    }, [router]);
-
     return (
-        <HomePage />
+        <div >
+            <main >
+                <Image
+                  
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={180}
+                    height={38}
+                    priority
+                />
+                <ol>
+                    <li>
+                        Get started by editing <code>src/app/page.tsx</code>.
+                    </li>
+                    <li>Save and see your changes instantly.</li>
+                </ol>
+
+                <div >
+                    <a
+                       
+                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Image
+                         
+                            src="/vercel.svg"
+                            alt="Vercel logomark"
+                            width={20}
+                            height={20}
+                        />
+                        Deploy now
+                    </a>
+                    <a
+                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                       
+                    >
+                        Read our docs
+                    </a>
+                </div>
+            </main>
+            <footer >
+                <a
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/file.svg"
+                        alt="File icon"
+                        width={16}
+                        height={16}
+                    />
+                    Learn
+                </a>
+                <a
+                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/window.svg"
+                        alt="Window icon"
+                        width={16}
+                        height={16}
+                    />
+                    Examples
+                </a>
+                <a
+                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/globe.svg"
+                        alt="Globe icon"
+                        width={16}
+                        height={16}
+                    />
+                    Go to nextjs.org →
+                </a>
+            </footer>
+        </div>
     );
 }
